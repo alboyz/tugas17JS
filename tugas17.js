082e1b8feb7fcd66fe5250bbbee4e547604b9cc7
ali <alboyzskeaters@gmail.com>
2020-01-10 07:22:43 +0700

    Tugas17JS

---
+++ tugas17.js
@@ -0,0 +1,6 @@
+let kalimat1 = "Saya beLajar bahaSa peMrograman deNgan khUsuk.";
+console.log(kalimat1.toLowerCase());
+console.log(kalimat1.toUpperCase());
+let kalimat2 = "Allen SedAng MemAncinG IkaN di SuNgai, DIA MEndapat IkAN GaBuS 3 Ekor dan LanGsuNg Dijual.";
+console.log(kalimat2.toLowerCase());
+console.log(kalimat2.toUpperCase());
